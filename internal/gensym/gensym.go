25c77f9456c523e406788c59cf46cd6676ccb607
Nicholas Wiersma <nick@wiersma.co.za>
2020-10-23 13:09:18 +0200

    fix: update yaegi and fix symgen

--- internal/gensym/gensym.go
+++ internal/gensym/gensym.go
@@ -1,36 +1,36 @@
 package main
 
-// import (
-//	"bytes"
-//	"flag"
-//	"io/ioutil"
-//	"log"
-//
-//	"github.com/traefik/yaegi/internal/extract"
-// )
-//
-// const (
-//	pkgIdent = "github.com/glasslabs/looking-glass/module/types"
-// )
-//
-// var (
-//	outputFile = flag.String("o", "gen.go", "The output file")
-// )
-//
-// func main() {
-//	flag.Parse()
-//
-//	ext := extract.Extractor{Dest: "types"}
-//
-//	var buf bytes.Buffer
-//	_, err := ext.Extract(pkgIdent, "", &buf)
-//	if err != nil {
-//		log.Println(err)
-//		return
-//	}
-//
-//	if err := ioutil.WriteFile(*outputFile, buf.Bytes(), 0600); err != nil {
-//		log.Println(err)
-//		return
-//	}
-// }
+import (
+	"bytes"
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"github.com/traefik/yaegi/extract"
+)
+
+const (
+	pkgIdent = "github.com/glasslabs/looking-glass/module/types"
+)
+
+var (
+	outputFile = flag.String("o", "gen.go", "The output file")
+)
+
+func main() {
+	flag.Parse()
+
+	ext := extract.Extractor{Dest: "types"}
+
+	var buf bytes.Buffer
+	_, err := ext.Extract(pkgIdent, "", &buf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err := ioutil.WriteFile(*outputFile, buf.Bytes(), 0600); err != nil {
+		log.Println(err)
+		return
+	}
+}
